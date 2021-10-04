d7c5cd44eb1a124989e730c3fa6b95acc121d70c
samurangel56 <55509759+samurangel56@users.noreply.github.com>
2021-10-04 08:22:56 -0500

    pp1

--- index.php
+++ index.php
@@ -3,3 +3,9 @@ $connectionInfo = array("UID" => "samu", "pwd" => "$4muD4t4", "Database" => "lab
 $serverName = "tcp:17051809.database.windows.net,1433";
 $conn = sqlsrv_connect($serverName, $connectionInfo);
 ?>
+<html>
+  <head></head>
+  <body>
+    <?php if($conn){echo "<p> exito </p>";} ?>
+  </body>
+</html>
