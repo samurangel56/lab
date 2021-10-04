4d77c7f2a8f96172680472dc9b8310496d09042f
Samuel Rangel <samu@iMac-de-Samuel.local>
2021-10-03 23:10:12 -0500

    init

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+try {
+    $conn = new PDO("sqlsrv:server = tcp:17051809.database.windows.net,1433; Database = laboratorio", "samu", "$4muD4t4");
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "exito";
+}
+catch (PDOException $e) {
+    print("Error connecting to SQL Server.");
+    die(print_r($e));
+}
+?>
