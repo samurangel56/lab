a52452867550fc865646ab9bec3e652c55960f29
samurangel56 <55509759+samurangel56@users.noreply.github.com>
2021-10-03 23:59:27 -0500

    p6

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 $connectionInfo = array("UID" => "samu", "pwd" => "$4muD4t4", "Database" => "laboratorio", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
 $serverName = "tcp:17051809.database.windows.net,1433";
 $conn = sqlsrv_connect($serverName, $connectionInfo);
-if($conn){echo 'exito';}
-else{echo 'fail';die( print_r( sqlsrv_errors(), true));}
-echo 'hola mundo';
+if($conn){echo "exito";}
+else{echo "fail";die( print_r( sqlsrv_errors(), true));}
+echo "hola mundo";
 ?>
