b73e05141283bc8dbbe19284f157637f92092b08
samurangel56 <55509759+samurangel56@users.noreply.github.com>
2021-10-04 00:02:40 -0500

    p7

--- index.php
+++ index.php
@@ -1,8 +1,10 @@
 <?php
-$connectionInfo = array("UID" => "samu", "pwd" => "$4muD4t4", "Database" => "laboratorio", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
-$serverName = "tcp:17051809.database.windows.net,1433";
-$conn = sqlsrv_connect($serverName, $connectionInfo);
-if($conn){echo "exito";}
-else{echo "fail";die( print_r( sqlsrv_errors(), true));}
-echo "hola mundo";
+try {
+    $conn = new PDO("sqlsrv:server = tcp:17051809.database.windows.net,1433; Database = laboratorio", "samu", "$4muD4t4");
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+catch (PDOException $e) {
+    print("Error connecting to SQL Server.");
+    die(print_r($e));
+}
 ?>
