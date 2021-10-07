7cd789ec7086010b0f73182b21346eb9b8e4e81b
Samuel Rangel <samu@iMac-de-Samuel.local>
2021-10-07 17:01:29 -0500

    conn

--- index.php
+++ index.php
@@ -2,12 +2,11 @@
 $connectionInfo = array("UID" => "samu", "pwd" => "$4muD4t4", "Database" => "laboratorio", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
 $serverName = "tcp:17051809.database.windows.net,1433";
 $conn = sqlsrv_connect($serverName, $connectionInfo);
+
+if( $conn ) {
+     echo "Conexión establecida.<br />";
+}else{
+     echo "Conexión no se pudo establecer.<br />";
+     die( print_r( sqlsrv_errors(), true));
+}
 ?>
-<html>
-  <head></head>
-  <body>
-    <?php
-      if($conn){echo "<p> exito </p>";}
-    ?>
-  </body>
-</html>
